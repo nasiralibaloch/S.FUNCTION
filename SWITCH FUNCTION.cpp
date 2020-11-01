387705fcc7312d1fce5f686c624059736544a66d
Nasir Ali Baloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-11-01 23:50:27 +0500

    Add files via upload

---
+++ SWITCH FUNCTION.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+int sum()
+{
+	int a=7;
+	int b=5;
+	int c;
+	c=a+b;
+	cout<<" the value c is ="<<c<<endl;
+}
+int sub()
+{
+	int a=76;
+	int b=43;
+	int c;
+	c=a-b;
+	cout<<"the value of c is="<<c<<endl;
+}
+int mul()
+{
+   	int a=76;
+	int b=43;
+	int c;
+	c=a*b;
+	cout<<"the value of c is="<<c<<endl;	
+}
+float div()
+{
+	float a=76.6;
+	float b=43.5;
+	float c;
+	 c=a/b;
+	cout<<"the value of c is="<<c<<endl;
+}
+int main()
+{
+	sum();
+	sub();
+	mul();
+	div();
+	char ch;
+	cout<<"enter the ch"<<endl;
+	cin>>ch;
+	switch(ch)
+	{
+		case'A':
+		       'A',sum();
+		       break;
+		case'B':
+			    'B',sub();
+			     break;
+		case'C':
+			    'C',mul();
+			      break;
+		case'D':
+			     'D',div();
+			       break;	     		     
+	}
+}
